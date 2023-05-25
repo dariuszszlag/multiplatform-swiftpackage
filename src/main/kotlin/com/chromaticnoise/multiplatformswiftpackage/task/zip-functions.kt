85ba81186884a3085eaa04e8692f23e535d99c70
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-05-25 23:55:25 +0200

    Use another command for zip file checksum

--- src/main/kotlin/com/chromaticnoise/multiplatformswiftpackage/task/zip-functions.kt
+++ src/main/kotlin/com/chromaticnoise/multiplatformswiftpackage/task/zip-functions.kt
@@ -7,6 +7,7 @@ import com.chromaticnoise.multiplatformswiftpackage.domain.PluginConfiguration
 import com.chromaticnoise.multiplatformswiftpackage.domain.VersionName
 import com.chromaticnoise.multiplatformswiftpackage.domain.ZipFileName
 import org.gradle.api.Project
+import org.gradle.process.ExecSpec
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.nio.charset.Charset
@@ -18,12 +19,7 @@ internal fun Project.zipFileChecksum(
     val os = ByteArrayOutputStream()
     return if (zipFile.exists()) {
         exec {
-            commandLine(
-                "swift",
-                "package",
-                "compute-checksum",
-                zipFile.path
-            )
+            getSha256(zipFile)
             standardOutput = os
         }
         os.toByteArray().toString(Charset.defaultCharset()).trim()
@@ -51,4 +47,17 @@ private fun Project.getZipFileBuilder(
     val zipFileNamed = zipFileName.getName(distributionMode, "${packageName.nameWithSuffix}-${versionName.value}")
     val zipFilePath = file("$outputPath/$zipFileNamed")
     return File(outputPath, zipFilePath.name)
+}
+
+private fun ExecSpec.getSha256(file: File): ExecSpec? {
+    val filePath = file.path
+    return commandLine(
+        "shasum",
+        "-a",
+        "256",
+        filePath,
+        "|",
+        "sed",
+        "'s/ .*//'"
+    )
 }
